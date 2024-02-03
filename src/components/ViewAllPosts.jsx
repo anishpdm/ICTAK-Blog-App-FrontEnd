acbd4698c6d622ed881f4f436ad207d3acdbe707
anishpdm <anish.vilayil.s@gmail.com>
2024-02-03 16:26:42 +0530

    create post completed

--- src/components/ViewAllPosts.jsx
+++ src/components/ViewAllPosts.jsx
@@ -6,7 +6,83 @@ const ViewAllPosts = () => {
     <div>
             <BlogHeader /> 
 
-ViewAllPosts</div>
+<div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12">
+
+            <div className="row g-3">
+                <div className="col col-12 col-sm-12 col-md-12">
+
+
+                <div class="card mb-3" >
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/25/46/33/b7/caption.jpg?w=500&h=400&s=1" class="img-fluid rounded-start" alt="..." />
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12">
+
+
+
+
+                <div class="card mb-3" >
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/25/46/33/b7/caption.jpg?w=500&h=400&s=1" class="img-fluid rounded-start" alt="..." />
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12">
+
+
+
+
+
+                <div class="card mb-3" >
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/25/46/33/b7/caption.jpg?w=500&h=400&s=1" class="img-fluid rounded-start" alt="..." />
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</div>
+
+
+</div>
   )
 }
 
